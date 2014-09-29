0784cbcc81604e5754376934efaa25c8758d420b
Ciki <matula.m@gmail.com>
2014-09-29 20:25:13 +0200

    update example

--- example.php
+++ example.php
@@ -1,26 +1,25 @@
 <?php
-
 // Config
 $repos = array(
 	'Digitilt' => array(
 		'branch' => 'labs',
 		'remote' => 'origin',
 		'path' => __DIR__ . '/../../../',
-		'post_deploy' => 'callback',
+//		'post_deploy' => 'callback',
 	),
 );
 
 
 // Registers all of our repos with the Deploy class
 require 'inc/class.deploy.php';
-foreach ($repos as $name => $repo)
+foreach ($repos as $name => $repo) {
 	Deploy::register_repo($name, $repo);
-
+}
 
 
 // Make sure we have a payload, stop if we do not.
 if (!isset($_POST['payload'])) {
-	die('<h1>No payload present</h1><p>A BitBucket POST payload is required to deploy from this script.</p>');
+	die('<h1>No payload present</h1><p>A POST payload is required to deploy from this script.</p>');
 }
 $payload = $_POST['payload'];
 
