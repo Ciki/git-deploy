742590303fe740582201a598ea8e00ce0e0274bc
Ciki <matula.m@gmail.com>
2014-09-29 21:11:57 +0200

    update example

--- example.php
+++ example.php
@@ -11,32 +11,44 @@ $repos = array(
 
 
 // Registers all of our repos with the Deploy class
-require 'inc/class.deploy.php';
+require __DIR__ . '/Deploy.php';
 foreach ($repos as $name => $repo) {
 	Deploy::register_repo($name, $repo);
 }
 
 
 // Make sure we have a payload, stop if we do not.
-if (!isset($_POST['payload'])) {
-	die('<h1>No payload present</h1><p>A POST payload is required to deploy from this script.</p>');
+$payload = getPayload();
+if (!$payload) {
+	die('<h1>No payload present</h1>');
 }
-$payload = $_POST['payload'];
 
 
 // https://developer.github.com/webhooks/#delivery-headers
 if (strncmp($_SERVER['User-Agent'], 'GitHub-Hookshot/', 16)) {
-	require 'GithubDeploy.php';
+	require __DIR__ . '/GithubDeploy.php';
 	$deploy = new GitHubDeploy($payload);
-
 // todo: how to detect Bitbucket hook service?
 } elseif (false) {
-	require 'BitBucketDeploy.php';
+	require __DIR__ . '/BitBucketDeploy.php';
 	$deploy = new BitBucketDeploy($payload);
 } else {
 	die('Unknown hook service!');
 }
 
 
-
 $deploy->execute();
+
+/**
+ * @return string|NULL JSON encoded payload
+ */
+function getPayload()
+{
+	if (isset($_POST['payload'])) {
+		$json = $_POST['payload'];
+	} else {
+		$json = file_get_contents('php://input');
+	}
+	return $json ? : NULL;
+}
+
